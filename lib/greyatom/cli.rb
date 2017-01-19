d1cfaa498508cc9f215da2038777a7fc3ead46d1
surajhell88 <surajhell88@gmail.com>
2017-01-19 12:29:07 +0530

    first commit

---
+++ lib/greyatom/cli.rb
@@ -0,0 +1,7 @@
+require "greyatom/cli/version"
+
+module Greyatom
+  module Cli
+    # Your code goes here...
+  end
+end
---
+++ spec/greyatom/cli_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Greyatom::Cli do
+  it "has a version number" do
+    expect(Greyatom::Cli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
